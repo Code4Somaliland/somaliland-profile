bc0fac5ae71c5d95e045d5658c5210d3792700b3
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-06-29 17:41:24 +0300

    Initial project setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# Somaliland Profile
+
+This website is built to display Somaliland Profile on the Web for easy access and wide reach.
+
+Country profile is developed by [Somaliland Ministry of Foriegn Affiars](https://twitter.com/somalilandmfa)
---
+++ docusaurus.config.js
@@ -0,0 +1,102 @@
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "Somaliland Profile",
+  tagline: "Republic of Somaliland",
+  url: "https://somalilandprofile.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  projectName: "somomalilandprofile", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "Somaliland Profile",
+      logo: {
+        alt: "Somaliland Logo",
+        src: "img/somaliland.svg",
+      },
+      items: [
+        {
+         to: "/home",
+          position: "left",
+          label:"About",
+        },
+      ],
+    },
+    footer: {
+      style: "dark",
+      links: [
+        {
+          title: "Docs",
+          items: [
+            {
+              label: "Tutorial",
+              to: "/docs/intro",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/facebook/docusaurus",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Somaliland, Country Profile.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: '/',
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/",
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+};
