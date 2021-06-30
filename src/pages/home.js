9776b4d78eb17c7d0168bc3cd9f0cc36c3d14bcc
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-06-30 15:24:22 +0300

    Add about page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import clsx from 'clsx';
+import { Box, Flex , Text, Stack} from "@chakra-ui/react";
+import {StarIcon } from '@chakra-ui/icons'
+
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
+
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
@@ -24,10 +26,45 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <Box bg="red" w="100%" p={32} color="white" />
+        <Flex
+          bg="white"
+          w="100%"
+          p={16}
+          color="white"
+          justify="center"
+          align="center"
+        >
+          <StarIcon style={{ color: "black" }} w={32} h={32} color="gray.500" />
+        </Flex>
+        <Box bg="green" w="100%" p={32} color="white" />
+        <Flex justify="center" my="3rem">
+          <Stack spacing={8}  maxW="48em">
+            <Text>
+              Somaliland has a long history that goes back thousands of years.
+              10,000 year old Neolithic paintings in Las Geel, near Hargeisa,
+              the capital, show that Somaliland was home to the earliest
+              civilizations in the world.
+            </Text>
+            <Text>
+              Somaliland, as part of the Land of Punt was a major trading
+              partner with ancient Egyptians as records during the reign of
+              Sahura of the Fifth Dynasty around 2250 BCE indicate. The coastal
+              cities of Zeila, Berbera and Maydh were deeply involved in spice
+              trade with the Romans.
+            </Text>
+            <Text>
+              In the 7th century, Islam was introduced to Somaliland, and by the
+              13th century had a thriving Islamic sultanate, Adal, whose main
+              port was Zeila. But the modern history of Somaliland started in
+              the 19th century as presented chronologically below:
+            </Text>
+          </Stack>
+        </Flex>
       </main>
     </Layout>
   );
