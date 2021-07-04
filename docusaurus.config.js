ecf4bd3c3d2cb5db5aa84da4d3f1ed3bb5993b68
Mukhtar Mahamed <mukhtar.mahomed@gmail.com>
2021-07-04 16:28:46 +0300

    Enable PWA

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,11 +19,11 @@ module.exports = {
         src: "img/somaliland.svg",
       },
       items: [
-          {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Country Profile',
+        {
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Country Profile",
         },
         {
           to: "/about",
@@ -106,15 +106,65 @@ module.exports = {
       },
     ],
   ],
-  // plugins: [
-  //   [
-  //     "@docusaurus/plugin-ideal-image",
-  //     {
-  //       quality: 70,
-  //       max: 1030,
-  //       min: 640,
-  //       steps: 2,
-  //     },
-  //   ],
-  // ],
+  plugins: [
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        debug: true,
+        offlineModeActivationStrategies: [
+          "appInstalled",
+          "standalone",
+          "queryString",
+        ],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/icons/icon-192x192.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/icons/icon-192x192.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/img/emblem.svg",
+            color: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/img/docusaurus.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
+    ],
+  ],
 };
